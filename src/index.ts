45c09d06ba0543bbb840612d4cef322e5628095e
Ishan Srivastava <ishan@tor.us>
2022-01-11 15:46:15 +0530

    feat: added basic structure of app, minify bundle left

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 // @ts-ignore added here cz @types/react-native giving weird bugs of repetitive declaration
 import { Linking } from 'react-native';
-import { TorusSolanaConfig } from './interface';
+import { ProviderConfig, TorusSolanaConfig } from './interface';
 import { defaultConfig } from './utils/constants';
 import { CallbackMsgType } from './utils/enum';
-import {encode as btoa} from 'base-64';
+import {encode as btoa,  decode as atob} from 'base-64';
 import { objectToQueryParams } from './utils/helper';
-
+import 'text-encoding-polyfill'
+// @ts-ignore added here cz @types/react-native giving weird bugs of repetitive declaration
+import { URLSearchParams } from 'whatwg-url';
+if (typeof BigInt === 'undefined') global.BigInt = require('big-integer')
 export default class TorusSolanaSdk { 
-    sdkConfig: TorusSolanaConfig = defaultConfig;
+    private _sdkConfig: TorusSolanaConfig = defaultConfig;
 
     constructor(config?: TorusSolanaConfig) {
         if (config) { 
-            this.sdkConfig = config;
+            this._sdkConfig = config;
         }
     }
     private _resultCallback = (event: any,  type?: CallbackMsgType) => {
@@ -22,61 +25,94 @@ export default class TorusSolanaSdk {
         this.openUrl("login");
     }
 
-    signMessage() { 
-        this.openUrl("sign_message");
+    logout() { 
+        this.openUrl("logout");
     }
 
-    private async openUrl(method: string) {
-        const baseurl = `${this.sdkConfig.base_url}/redirectflow`;
+    getUserInfo() { 
+        this.openUrl("user_info");
+    }
+
+    setProvider(providerConfig: ProviderConfig) { 
+        this.openUrl("set_provider", providerConfig);
+    }
+
+    getProviderState() { 
+        this.openUrl("get_provider_state");
+    }
+
+    walletGetProviderState() { 
+        this.openUrl("wallet_get_provider_state");
+    }
+
+    topup(payload: { selectedAddress: string,provider: string}) { 
+        this.openUrl("topup", payload);
+    }
+    
+    signTransaction(serializedTransaction: string) { 
+        this.openUrl("sign_transaction", serializedTransaction);
+    }
+
+    signMessage(message: Uint8Array) { 
+        this.openUrl("sign_message", message);
+    }
+    
+    sendTransaction(serializedTransaction: string) { 
+        this.openUrl("send_transaction", serializedTransaction);
+    }
+
+    getGaslessPublicKey() { 
+        this.openUrl("get_gasless_public_key");
+    }
+
+    listNft() { 
+        this.openUrl("nft_list");
+    }
+
+    sendNft(transactionData: {mint_add: string,receiver_add: string,sender_add: string}) { 
+        this.openUrl("nft_transfer", transactionData);
+    }
+
+    iframeStatus(iframeData: {isFullScreen: boolean,rid: string}) { 
+        this.openUrl("iframe_status", iframeData);
+    }
+
+    private async openUrl(method: string, data?: any) {
+        const baseurl = `${this._sdkConfig.base_url}/redirectflow`;
         let url = baseurl;
         let params = {};
         switch (method) {
-            // case "set_provider":
-            //     params = {
-            //         blockExplorerUrl: "https://explorer.solana.com",
-            //         chainId: "0x1",
-            //         displayName: "Solana Mainnet",
-            //         logo: "solana.svg",
-            //         rpcTarget: solanaWeb3.clusterApiUrl("mainnet-beta"),
-            //         ticker: "SOL",
-            //         tickerName: "Solana Token",
-            //     };
-            //     break;
+            case "set_provider":
+                params = { ...data };
+                break;
             case "iframe_status":
-                params = {
-                    isFullScreen: false,
-                    rid: "",
-                };
+                params = {...data};
                 break;
             case "topup":
                 params = {
                     params: {
-                        selectedAddress: "C4Letg829ytf5PqyEDSdBUWs4T1GT7whYGrZsJreftgW",
+                        selectedAddress: data.selectedAddress,
                     },
-                    provider: "rampnetwork",
+                    provider: data.provider,
                 };
                 break;
-            // case "send_transaction":
-            //     params = {
-            //         message: await generateTransaction(),
-            //     };
-            //     break;
-            // case "sign_transaction":
-            //     params = {
-            //         message: await generateTransaction(),
-            //     };
-            //     break;
+            case "send_transaction":
+                params = {
+                    message: data,
+                };
+                break;
+            case "sign_transaction":
+                params = {
+                    message: data,
+                };
+                break;
             case "sign_message":
                 params = {
-                    data: (new TextEncoder()).encode("Example Message"),
+                    data,
                 };
                 break;
             case "nft_transfer":
-                params = {
-                    mint_add: "BAYYCCY31SRrexQKwGqDRonVzMiB2Y2HagNZQv6cNWk7",
-                    receiver_add: "C4Letg829ytf5PqyEDSdBUWs4T1GT7whYGrZsJreftgW",
-                    sender_add: "C4Letg829ytf5PqyEDSdBUWs4T1GT7whYGrZsJreftgW"
-                };
+                params = { ...data };
                 break;
             default:
         }
@@ -87,7 +123,7 @@ export default class TorusSolanaSdk {
         if ((typeof params === "object" && Object.keys(params).length === 0) || (Array.isArray(params) && params.length === 0)) {
             useParams = false;
         }
-        const resolvePath = `${this.sdkConfig.deeplink_schema}://redirect-handle`;
+        const resolvePath = `${this._sdkConfig.deeplink_schema}://redirect-handle`;
         url = `${url}?${objectToQueryParams(queryParams)}&resolveRoute=${resolvePath}${useParams ? "#params=" + encodedParams : ""
             }`;
         Linking.openURL(url).catch((err: any) => this._resultCallback("Error openeing URL", CallbackMsgType.ERROR));
@@ -95,8 +131,8 @@ export default class TorusSolanaSdk {
 
     getResults(linkingObject: any, callback: (event: any, type?: CallbackMsgType) => void) { 
         this._resultCallback = callback;
-        linkingObject.addEventListener('url', (data: any) => { 
-            callback(data, CallbackMsgType.SUCCESS)
+        linkingObject.addEventListener('url', (resultUrl: any) => { 
+            callback({result : atob(`${new URLSearchParams(resultUrl.url).get('result')}`) || '', method: new URLSearchParams(resultUrl.url).get('method')}, CallbackMsgType.SUCCESS)
         });
     }
 }
--- src/interface.ts
+++ src/interface.ts
@@ -2,3 +2,12 @@ export interface TorusSolanaConfig {
     base_url: string
     deeplink_schema: string
 }
+export interface ProviderConfig {
+    blockExplorerUrl: string;
+    logo: string;
+    tickerName: string;
+    ticker: string;
+    rpcTarget: string;
+    chainId: string;
+    displayName: string;
+}
